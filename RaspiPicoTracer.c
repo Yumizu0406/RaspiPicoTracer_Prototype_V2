ee9b9965c3d03bde4034473bffbf38529de33496
yuusukeShimizu <shimizu.yasurikanazuti@gmail.com>
2024-09-09 21:38:39 +0900

    周期処理追加

--- RaspiPicoTracer.c
+++ RaspiPicoTracer.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "driver/cycle.h"
 
+void init_driver(void);
 
 int main()
 {
     stdio_init_all();
+    init_driver();
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+void init_driver(void){
+    //init_sw();
+    //init_lineSensor();
+    init_cycle();
 }
